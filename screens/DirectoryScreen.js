b58faf57c9598dace5db269e412474472ff9ff06
sabrinaMac <sabrina.scherma@gmail.com>
2023-01-28 10:21:26 -0700

    added favorites slice

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,18 +1,30 @@
+import { FlatList, Text, View } from 'react-native'
+import { Tile } from 'react-native-elements'
 import { useSelector } from 'react-redux'
 import { baseUrl } from '../shared/baseUrl'
-import { FlatList } from 'react-native'
-import { Tile } from 'react-native-elements'
+import Loading from '../components/LoadingComponent'
 
 const DirectoryScreen = ({ navigation }) => {
   const campsites = useSelector((state) => state.campsites)
 
+  if (campsites.isLoading) {
+    return <Loading />
+  }
+  if (campsites.errMess) {
+    return (
+      <View>
+        <Text>{campsites.errMess}</Text>
+      </View>
+    )
+  }
+
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
       <Tile
-        onPress={() => navigation.navigate('CampsiteInfo', { campsite })}
         title={campsite.name}
         caption={campsite.description}
         featured
+        onPress={() => navigation.navigate('CampsiteInfo', { campsite })}
         imageSrc={{ uri: baseUrl + campsite.image }}
       />
     )
